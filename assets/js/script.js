151f2f31a918f1061e7b2117592b30d2f3e3a226
programmer-1991 <>
2023-05-11 12:23:36 +0000

    style next-button and create 'questions' constant in javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,60 @@
-console.log("connected");
+/*create a constant "questions" to add our questions and the options for the answers.*/ 
+const questions = [
+    {
+        question: "question-1",
+        answers: [
+            { text: "answer1", correct: false },
+            { text: "answer2", correct: true },
+            { text: "answer3", correct: false },
+            { text: "answer4", correct: false },
+        ],
+
+    },
+
+    {
+        question: "question-2",
+        answers: [
+
+            { text: "answer5", correct: true },
+            { text: "answer5", correct: false },
+            { text: "answer5", correct: false },
+            { text: "answer5", correct: false },
+
+        ]
+    },
+    {
+        question: "question-3",
+        answers: [
+
+            { text: "answer6", correct: false },
+            { text: "answer6", correct: false },
+            { text: "answer6", correct: false },
+            { text: "answer6", correct: true },
+
+        ]
+    },
+
+    {
+        question: "question-4",
+        answers: [
+
+            { text: "answer7", correct: true },
+            { text: "answer7", correct: false },
+            { text: "answer7", correct: false },
+            { text: "answer7", correct: false },
+
+        ]
+    },
+
+    {
+        question: "question-5",
+        answers: [
+
+            { text: "answer8", correct: true },
+            { text: "answer8", correct: true },
+            { text: "answer8", correct: true },
+            { text: "answer8", correct: true },
+
+        ]
+    }
+];
