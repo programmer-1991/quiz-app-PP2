065e3325e86779cf4eac7a124f9e66589f990e0c
programmer-1991 <>
2023-05-12 01:56:17 +0000

    uppdate questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,59 @@
 /*create a constant "questions" to add our questions and the options for the answers.*/
 const questions = [
     {
-        question: "question-1",
+    question: "How many minutes are in a full week? ",
         answers: [
-            { text: "answer3", correct: false },
-            { text: "answer3", correct: true },
-            { text: "answer3", correct: false },
-            { text: "answer3", correct: false },
+            { text: "7500", correct: false },
+            { text: "10080", correct: true },
+            { text: "11000", correct: false },
+            { text: "9800", correct: false },
         ],
 
     },
 
     {
-        question: "question-2",
+      question: "What city is known as The Eternal City? ",
         answers: [
 
-            { text: "answer5", correct: true },
-            { text: "answer5", correct: false },
-            { text: "answer5", correct: false },
-            { text: "answer5", correct: false },
+            { text: "Rome", correct: true },
+            { text: "London", correct: false },
+            { text: "New york", correct: false },
+            { text: "Tokyo", correct: false },
 
         ]
     },
     {
-        question: "question-3",
+      question: "Which planet has the most moons?",
         answers: [
 
-            { text: "answer6", correct: false },
-            { text: "answer6", correct: false },
-            { text: "answer6", correct: false },
-            { text: "answer6", correct: true },
-
-        ]
-    },
-
-    {
-        question: "question-4",
-        answers: [
-
-            { text: "answer7", correct: true },
-            { text: "answer7", correct: false },
-            { text: "answer7", correct: false },
-            { text: "answer7", correct: false },
+            { text: "Venus", correct: false },
+            { text: "Mars", correct: false },
+            { text: "Jupiter", correct: false },
+            { text: "Saturn", correct: true },
 
         ]
     },
 
     {
-        question: "question-5",
+      question: "Which language has the more native speakers: English or Spanish?",
         answers: [
 
-            { text: "answer8", correct: false},
-            { text: "answer8", correct: false},
-            { text: "answer8", correct: false},
-            { text: "answer8", correct: true},
+            { text: "Spanish", correct: true },
+            { text: "English", correct: false },
+            { text: "Chineese", correct: false },
+            { text: "Frensh", correct: false },
+
+        ]
+    },
+
+    {
+      question: "What country drinks the most coffee per capita?",
+        answers: [
+
+            { text: "South Korea", correct: false},
+            { text: "Sweden", correct: false},
+            { text: "Spain", correct: false},
+            { text: "Finland", correct: true},
 
         ]
     }
